701d022c0e7580f76e2b8e93f260cf26fe4ac8ee
Haroun Ajisegiri <63101614+hajisegiri@users.noreply.github.com>
2023-11-27 20:45:19 -0500

    Create athleisure_app.py

---
+++ athleisure_app.py
@@ -0,0 +1,8 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title("Zena's Amazing Athleisure Catalog")
+streamlit.header("Pick a sweatsuit color or style:")
